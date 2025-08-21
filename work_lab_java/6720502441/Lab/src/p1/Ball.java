0c007c9c14b1c26bb15c55c0a706ca670927592b
jackpod019 <jackpod019@gmail.com>
2025-08-21 17:54:07 +0700

    Add files via upload

---
+++ work_lab_java/6720502441/Lab/src/p1/Ball.java
@@ -0,0 +1,12 @@
+package p1;
+
+abstract  class Ball {
+String treadMark;
+public Ball(String treadMark) {
+	this.treadMark=treadMark;
+}
+public abstract void inflate(double x) {
+	
+}
+}
+
---
+++ work_lab_java/6720502441/Lab/src/p1/BallA.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class BallA extends Ball {
+
+	public void inflate(double x) {
+		
+	}
+	
+}
+
+public class BallA implement RollAble{
+	public  void roll() {
+		
+	}
+}
